1a447e9bba0a413b80f6df26b907a5d4536bd72d
Riddhi Saraf <143543865+riddhisf@users.noreply.github.com>
2026-04-11 12:16:13 +0530

    Delete ProductService directory

--- ProductService/src/main/java/com/example/productservice/exception/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.productservice.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-
-	public ProductNotFoundException(String message) {
-		super(message);
-	}
-}
